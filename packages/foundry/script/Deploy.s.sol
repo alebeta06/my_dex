753de1b34d63a8a2aa5fe332511d9ff36a1fdf03
alebeta <alebeta0000@gmail.com>
2025-04-18 00:57:55 -0500

    Enhance documentation for the main deployment script

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -4,9 +4,12 @@ pragma solidity ^0.8.19;
 import "./DeployHelpers.s.sol";
 import { DeployVault } from "./DeployVault.s.sol";
 
+/**
+ * @notice Main deployment script for all contracts
+ * @dev Run this when you want to deploy múltiples contratos de una vez
+ */
 contract DeployScript is ScaffoldETHDeploy {
     function run() external {
-
         DeployVault deployVault = new DeployVault();
         deployVault.run();
     }
