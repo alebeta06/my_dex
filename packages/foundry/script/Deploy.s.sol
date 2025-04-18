f0bd78aac5bcea8df55c30d7ddd2bcdb148c1300
alebeta <alebeta0000@gmail.com>
2025-04-18 00:53:46 -0500

    Remove SE2Token contract and its deployment script.

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -2,13 +2,10 @@
 pragma solidity ^0.8.19;
 
 import "./DeployHelpers.s.sol";
-import { DeploySE2Token } from "./DeploySE2Token.s.sol";
 import { DeployVault } from "./DeployVault.s.sol";
 
 contract DeployScript is ScaffoldETHDeploy {
     function run() external {
-        DeploySE2Token deploySE2Token = new DeploySE2Token();
-        deploySE2Token.run();
 
         DeployVault deployVault = new DeployVault();
         deployVault.run();
